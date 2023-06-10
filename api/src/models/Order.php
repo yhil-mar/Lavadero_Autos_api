8d4a4e81a8636aa66fe8cf4a725ee070ef377a38
yhil-mar <miniyasc@gmail.com>
2023-06-10 17:20:29 -0300

    modularización getOrders

--- api/src/models/Order.php
+++ api/src/models/Order.php
@@ -116,6 +116,132 @@ class Order extends Model {
             }
 
         }
+
+        public function fixOrders($ordersArray) {
+
+            $finalArray = [];
+
+                // $licensePlates = array_values(array_unique(array_column($result, 'carId')));
+
+                $previousLicense = null;
+
+                $previousService = null;
+
+                $previousOrder = null;
+
+                $contadorVehiculos = -1;
+                
+                foreach ($ordersArray as $service) {
+
+                    $currentLicense = $service['carId'];
+
+                    $currentService = $service['serviceId'];
+                    
+                    $currentWorker = $service['workerId'];
+
+                    $currentOrder = $service['orderService'];
+
+                    if ($currentLicense != $previousLicense) {
+                        
+                        $contadorVehiculos = $contadorVehiculos + 1;
+                        
+                        $contadorServicios = -1;
+
+                        $contadorServicios = $contadorServicios + 1;
+
+                        $sql = "SELECT * FROM cars WHERE licensePlate = '$currentLicense'";
+                        
+                        $vehicleResult = $this->query($sql);
+                        
+                        $vehicleResult = $vehicleResult->first();
+                        
+                        $finalArray[] = $vehicleResult;
+                        
+                        $finalArray[$contadorVehiculos]['services'] = array();
+                        
+
+                        $sql = "SELECT serviceName, cost FROM services WHERE id = '$currentService'";
+
+                        $serviceResult = $this->query($sql);
+                        
+                        $serviceResult = $serviceResult->first();
+                        
+                        $finalArray[$contadorVehiculos]['services'][] = $serviceResult;
+                        
+                        $finalArray[$contadorVehiculos]['services'][0]['workers'] = array();
+                        
+                        
+                        $sql = "SELECT name FROM workers WHERE rut_passport = '$currentWorker'";
+                        
+                        $workerResult = $this->query($sql);
+                        
+                        $workerResult = array_values($workerResult->first());
+                        
+                        $finalArray[$contadorVehiculos]['services'][0]['workers'] = $workerResult;
+                        
+
+                        $serviceDate = $service['orderDay'] . '-' . $service['orderMonth'] . '-' . $service['orderYear'] . ' ' . $service['orderHour'];
+
+                        $finalArray[$contadorVehiculos]['services'][0]['date'] = $serviceDate;
+
+
+                        $previousLicense = $currentLicense;
+
+                        $previousService = $currentService;
+
+                    } else {
+
+                        if ($currentService != $previousService || $currentOrder != $previousOrder) {
+
+                            $contadorServicios = $contadorServicios + 1;
+
+                            $sql = "SELECT serviceName, cost FROM services WHERE id = '$currentService'";
+
+                            $serviceResult = $this->query($sql);
+                                    
+                            $serviceResult = $serviceResult->first();
+
+                            array_push($finalArray[$contadorVehiculos]['services'], $serviceResult);
+        
+                            $finalArray[$contadorVehiculos]['services'][$contadorServicios]['workers'] = array();
+
+
+                            $sql = "SELECT name FROM workers WHERE rut_passport = '$currentWorker'";
+
+                            $workerResult = $this->query($sql);
+                                    
+                            $workerResult = array_values($workerResult->first());
+    
+                            $finalArray[$contadorVehiculos]['services'][$contadorServicios]['workers'] = $workerResult;
+
+
+                            $serviceDate = $service['orderDay'] . '-' . $service['orderMonth'] . '-' . $service['orderYear'] . ' ' . $service['orderHour'];
+
+                            $finalArray[$contadorVehiculos]['services'][$contadorServicios]['date'] = $serviceDate;
+
+
+                            $previousService = $currentService;
+
+                        } else {
+
+                            $sql = "SELECT name FROM workers WHERE rut_passport = '$currentWorker'";
+
+                            $workerResult = $this->query($sql);
+                                    
+                            $workerResult = array_values($workerResult->first());
+
+                            array_push($finalArray[$contadorVehiculos]['services'][$contadorServicios]['workers'], $workerResult[0]);
+
+                        }
+
+                    }
+
+                    $previousOrder = $currentOrder;
+                        
+                }
+
+                return $finalArray;
+        }
         
     }
 

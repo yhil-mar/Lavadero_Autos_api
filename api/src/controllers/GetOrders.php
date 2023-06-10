d801e2c5404ca8cc1c4865d6f9378f051b62cb14
yhil-mar <miniyasc@gmail.com>
2023-06-09 21:23:59 -0300

    complemento getOrders

--- api/src/controllers/GetOrders.php
+++ api/src/controllers/GetOrders.php
@@ -24,7 +24,67 @@
 
             if (count($result) > 0) {
 
-                return $result;
+                $finalArray = [];
+
+                $contador = 0;
+
+                foreach ($result as $service) {
+
+                    foreach ($service as $serviceKey => $serviceValue) {
+
+                        switch($serviceKey) {
+                            
+                            case 'carId':
+                                
+                                $sql = "SELECT * FROM cars WHERE licensePlate = '$serviceValue'";
+                                
+                                // echo ($serviceValue);
+                                $vehicleResult = $orderModel->query($sql);
+
+                                $vehicleResult = $vehicleResult->first();
+
+                                $finalArray[] = $vehicleResult;
+
+                                $finalArray[$contador]['services'] = array();
+    
+                                break;
+                            
+                            case 'serviceId':
+
+                                $sql = "SELECT serviceName, cost FROM services WHERE id = '$serviceValue'";
+
+                                $serviceResult = $orderModel->query($sql);
+                                
+                                $serviceResult = $serviceResult->first();
+
+                                $finalArray[$contador]['services'][] = $serviceResult;
+
+                                // $finalArray[$contador]['services']['workers'] = array();
+
+                                break;
+                            
+                            case 'workerId':
+
+                                
+
+                                break;
+    
+                            default:
+
+                                break;
+                        }
+
+                        
+                    }
+                    
+                    $contador = $contador + 1;
+                    
+                    // echo print_r($service);
+                }
+                
+                return $finalArray;
+
+                // return $result;
 
             } else {
 

810e4ed527fbe32aa0f68614b48f23816f1de408
yhil-mar <miniyasc@gmail.com>
2023-06-15 12:53:31 -0300

    orden tablas

--- api/src/creationTables/WorkersTable.php
+++ api/src/creationTables/WorkersTable.php
@@ -16,9 +16,9 @@ class WorkersTable {
             name VARCHAR(25) UNIQUE NOT NULL,
             address VARCHAR(25) NOT NULL,
             profitPercentage INT(3) NOT NULL,
+            percentageAfterGoal INT(3) NOT NULL,
             goal INT(10) NOT NULL,
             branch VARCHAR(15) NOT NULL,
-            percentageAfterGoal INT(3) NOT NULL,
             statusWorker ENUM('active', 'vacation', 'inactive') DEFAULT 'active'
             
         )";

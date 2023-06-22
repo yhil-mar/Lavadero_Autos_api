baf5caef639362e213e536469a8ebda18689ce07
yhil-mar <miniyasc@gmail.com>
2023-06-22 16:54:13 -0300

    fix getOrdersByDate with all querys

--- api/src/controllers/GetOrdersByDate.php
+++ api/src/controllers/GetOrdersByDate.php
@@ -12,60 +12,57 @@
 
             if (isset($query) && !empty($query)) {
 
-                $startDate = $query['startDate'];
+                // $startDate = $query['startDate'];
 
-                $endDate = $query['endDate'];
+                // $endDate = $query['endDate'];
 
-                $startYear = substr($startDate, 0, 4);
-                $startMonth = substr($startDate, 4, 2);
-                $startDay = substr($startDate, 6, 2);
-
-                $endYear = substr($endDate, 0, 4);
-                $endMonth = substr($endDate, 4, 2);
-                $endDay = substr($endDate, 6, 2);
-
-                $sql = "SELECT * FROM orders 
-                WHERE orderStatus = 'completed' 
-                AND orderYear >= $startYear
-                AND orderYear <= $endYear
-                AND orderMonth >= $startMonth
-                AND orderMonth <= $endMonth
-                AND orderDay >= $startDay
-                AND orderDay <= $endDay";
-
-
-                $result = $orderModel->query($sql);
-
-                if (!is_array($result)) {
-
-                    $result = $result->get();
-
-                    $previousOrder = 0;
-                    
-                    $invoiced = 0;
-
-                    foreach($result as $order){
-
-                        $currentOrder = $order['orderService'];
-
-                        if($currentOrder != $previousOrder){
-
-                            $invoiced = $invoiced + $order['invoiced'];
-
-                        }
-                        
-                        $previousOrder = $currentOrder;
-                        
-                    }
-
-                    return $invoiced;
-
-                } else {
-
-                    return $result;
                 
-                }                     
-           
+                // $month = $query['orderMonth']
+                
+                // $startYear = substr($startDate, 0, 4);
+                // $startMonth = substr($startDate, 4, 2);
+                // $startDay = substr($startDate, 6, 2);
+                
+                // $endYear = substr($endDate, 0, 4);
+                // $endMonth = substr($endDate, 4, 2);
+                // $endDay = substr($endDate, 6, 2);
+                
+                // $sql = "SELECT * FROM orders 
+                // WHERE orderStatus = 'completed' 
+                // AND orderYear >= $startYear
+                // AND orderYear <= $endYear
+                // AND orderMonth >= $startMonth
+                // AND orderMonth <= $endMonth
+                // AND orderDay >= $startDay
+                // AND orderDay <= $endDay";
+                
+                
+                // $result = $orderModel->query($sql);
+
+                $result = $orderModel->findOrders($query);
+                
+                // $result = $result->get();
+
+                $previousOrder = 0;
+                
+                $invoiced = 0;
+
+                foreach($result as $order){
+
+                    $currentOrder = $order['orderService'];
+
+                    if($currentOrder != $previousOrder){
+
+                        $invoiced = $invoiced + $order['invoiced'];
+
+                    }
+                        
+                    $previousOrder = $currentOrder;
+                        
+                }
+
+                return $invoiced;
+                           
             }
             
         }
